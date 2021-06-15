b9b3a8d9e622a141d080a62ff327bede48c06d24
Gideon Emeka <chuksgid@gmail.com>
2021-06-15 07:51:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# magic_express_delivery
+ Repositiory for the mobile application of Magic Express Delivery
---
+++ android/app/src/main/kotlin/com/octacoresoftwares/magic_express_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.octacoresoftwares.magic_express_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
