d76f4eab81a369e386fb92518031f22e4405154c
Gideon Emeka <chuksgid@gmail.com>
2021-10-17 17:11:43 +0100

    Update history_page.dart

--- lib/src/pages/history/views/history_page.dart
+++ lib/src/pages/history/views/history_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:magic_express_delivery/src/app/app.dart';
 import 'package:magic_express_delivery/src/models/models.dart';
 import 'package:magic_express_delivery/src/pages/pages.dart';
 import 'package:magic_express_delivery/src/utils/currency_converter.dart';
@@ -8,144 +9,154 @@ import 'package:repositories/repositories.dart';
 class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Order History',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline4
-                    ?.copyWith(fontWeight: FontWeight.w700),
-              ),
-              const SizedBox(width: 24),
-              BlocSelector<HistoryBloc, HistoryState, Status>(
-                selector: (s) => s.status,
-                builder: (_, status) {
-                  if (status == Status.loading) {
-                    return SizedBox(
-                      height: 24,
-                      width: 24,
-                      child: CircularProgressIndicator.adaptive(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.blue.shade900),
+    return BlocListener<AppBloc, AppState>(
+      listener: (context, state) {
+        if (state.order.orderId.isNotEmpty) {
+          final action = HistoryActions.fetchHistoryDetail;
+          final event = HistoryEvent(action, state.order);
+          context.read<HistoryBloc>().add(event);
+          Navigator.of(context).push(HistoryDetailPage.route(context));
+        }
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Order History',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      ?.copyWith(fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(width: 24),
+                BlocSelector<HistoryBloc, HistoryState, Status>(
+                  selector: (s) => s.status,
+                  builder: (_, status) {
+                    if (status == Status.loading) {
+                      return SizedBox(
+                        height: 24,
+                        width: 24,
+                        child: CircularProgressIndicator.adaptive(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.blue.shade900),
+                        ),
+                      );
+                    }
+                    return SizedBox.shrink();
+                  },
+                )
+              ],
+            ),
+            BlocBuilder<HistoryBloc, HistoryState>(
+              builder: (_, state) {
+                if (!state.noHistory) {
+                  return Expanded(
+                    child: RefreshIndicator(
+                      displacement: 120,
+                      onRefresh: () =>
+                          context.read<HistoryBloc>().refreshHistoryList(),
+                      child: CustomScrollView(
+                        slivers: [
+                          SliverToBoxAdapter(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColorDark,
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              padding: EdgeInsets.all(8),
+                              margin: EdgeInsets.only(top: 24, bottom: 12),
+                              child: Text(
+                                'Active Orders',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle1
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                          SliverList(
+                            delegate:
+                                SliverChildBuilderDelegate((context, index) {
+                              return buildItem(
+                                  context, state.activeOrders[index]);
+                            }, childCount: state.activeOrders.length),
+                          ),
+                          SliverToBoxAdapter(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColorDark,
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              padding: EdgeInsets.all(8),
+                              margin: EdgeInsets.only(top: 24, bottom: 12),
+                              child: Text(
+                                'Inactive Orders',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle1
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                          SliverList(
+                            delegate:
+                                SliverChildBuilderDelegate((context, index) {
+                              return buildItem(
+                                  context, state.inActiveOrders[index]);
+                            }, childCount: state.inActiveOrders.length),
+                          ),
+                        ],
                       ),
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-              )
-            ],
-          ),
-          BlocBuilder<HistoryBloc, HistoryState>(
-            builder: (_, state) {
-              if (!state.noHistory) {
-                return Expanded(
-                  child: RefreshIndicator(
-                    displacement: 120,
-                    onRefresh: () =>
-                        context.read<HistoryBloc>().refreshHistoryList(),
-                    child: CustomScrollView(
-                      slivers: [
-                        SliverToBoxAdapter(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColorDark,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            padding: EdgeInsets.all(8),
-                            margin: EdgeInsets.only(top: 24, bottom: 12),
-                            child: Text(
-                              'Active Orders',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .subtitle1
-                                  ?.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ),
-                        ),
-                        SliverList(
-                          delegate:
-                          SliverChildBuilderDelegate((context, index) {
-                            return buildItem(
-                                context, state.activeOrders[index]);
-                          }, childCount: state.activeOrders.length),
-                        ),
-                        SliverToBoxAdapter(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColorDark,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            padding: EdgeInsets.all(8),
-                            margin: EdgeInsets.only(top: 24, bottom: 12),
-                            child: Text(
-                              'Inactive Orders',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .subtitle1
-                                  ?.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ),
-                        ),
-                        SliverList(
-                          delegate:
-                          SliverChildBuilderDelegate((context, index) {
-                            return buildItem(
-                                context, state.inActiveOrders[index]);
-                          }, childCount: state.inActiveOrders.length),
-                        ),
-                      ],
                     ),
+                  );
+                }
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 24),
+                      Icon(
+                        Icons.warning_amber_rounded,
+                        size: 96,
+                        color: Colors.red,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'No data currently available to display',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16),
+                      TextButton(
+                        onPressed: () {
+                          final action = HistoryActions.onRefreshHistoryList;
+                          final event = HistoryEvent(action);
+                          context.read<HistoryBloc>().add(event);
+                        },
+                        child: Text(
+                          'Click to refresh page',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                      )
+                    ],
                   ),
                 );
-              }
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 24),
-                    Icon(
-                      Icons.warning_amber_rounded,
-                      size: 96,
-                      color: Colors.red,
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'No data currently available to display',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6
-                          ?.copyWith(fontWeight: FontWeight.w700),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {
-                        final action = HistoryActions.onRefreshHistoryList;
-                        final event = HistoryEvent(action);
-                        context.read<HistoryBloc>().add(event);
-                      },
-                      child: Text(
-                        'Click to refresh page',
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                    )
-                  ],
-                ),
-              );
-            },
-          )
-        ],
+              },
+            )
+          ],
+        ),
       ),
     );
   }
