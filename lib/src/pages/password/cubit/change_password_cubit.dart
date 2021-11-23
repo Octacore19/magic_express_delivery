c4de254e30649f00e26dd5b2f87b52c7a212b282
Chukwu Gideon Emeka <chuksgid@gmail.com>
2021-11-23 20:28:43 +0100

    Change password implemented

--- lib/src/pages/password/cubit/change_password_cubit.dart
+++ lib/src/pages/password/cubit/change_password_cubit.dart
@@ -6,6 +6,10 @@ import 'package:repositories/repositories.dart';
 
 class ChangePasswordState extends Equatable {
   const ChangePasswordState._({
+    required this.loading,
+    required this.oldPasswordObscured,
+    required this.newPasswordObscured,
+    required this.confirmPasswordObscured,
     required this.status,
     required this.oldPassword,
     required this.newPassword,
@@ -14,6 +18,10 @@ class ChangePasswordState extends Equatable {
 
   factory ChangePasswordState.init() {
     return ChangePasswordState._(
+      loading: false,
+      confirmPasswordObscured: true,
+      newPasswordObscured: true,
+      oldPasswordObscured: true,
       status: FormzStatus.pure,
       oldPassword: Password.pure(),
       newPassword: Password.pure(),
@@ -21,18 +29,31 @@ class ChangePasswordState extends Equatable {
     );
   }
 
+  final bool loading;
+  final bool oldPasswordObscured;
+  final bool newPasswordObscured;
+  final bool confirmPasswordObscured;
   final FormzStatus status;
   final Password oldPassword;
   final Password newPassword;
   final ConfirmPassword confirmNew;
 
   ChangePasswordState copyWith({
+    bool? loading,
+    bool? oldPasswordObscured,
+    bool? newPasswordObscured,
+    bool? confirmPasswordObscured,
     FormzStatus? status,
     Password? oldPassword,
     Password? newPassword,
     ConfirmPassword? confirmNew,
   }) {
     return ChangePasswordState._(
+      loading: loading ?? this.loading,
+      oldPasswordObscured: oldPasswordObscured ?? this.oldPasswordObscured,
+      newPasswordObscured: newPasswordObscured ?? this.newPasswordObscured,
+      confirmPasswordObscured:
+          confirmPasswordObscured ?? this.confirmPasswordObscured,
       status: status ?? this.status,
       oldPassword: oldPassword ?? this.oldPassword,
       newPassword: newPassword ?? this.newPassword,
@@ -41,20 +62,34 @@ class ChangePasswordState extends Equatable {
   }
 
   @override
-  List<Object?> get props => [status, oldPassword, newPassword, confirmNew];
+  List<Object?> get props => [
+        loading,
+        oldPasswordObscured,
+        newPasswordObscured,
+        confirmPasswordObscured,
+        status,
+        oldPassword,
+        newPassword,
+        confirmNew
+      ];
 }
 
 class ChangePasswordCubit extends Cubit<ChangePasswordState> {
   ChangePasswordCubit({
     required ErrorHandler errorHandler,
     required MiscRepo miscRepo,
-  }) : super(ChangePasswordState.init());
+  })  : _repo = miscRepo,
+        _handler = errorHandler,
+        super(ChangePasswordState.init());
+
+  final ErrorHandler _handler;
+  final MiscRepo _repo;
 
   void onOldPasswordChanged(String value) {
     final password = Password.dirty(value);
     final s = state.copyWith(
       oldPassword: password.valid ? password : Password.pure(value),
-      status: Formz.validate([password, state.oldPassword]),
+      status: Formz.validate([password, state.newPassword, state.confirmNew]),
     );
     emit(s);
   }
@@ -63,7 +98,7 @@ class ChangePasswordCubit extends Cubit<ChangePasswordState> {
     final password = Password.dirty(value);
     final s = state.copyWith(
       newPassword: password.valid ? password : Password.pure(value),
-      status: Formz.validate([password, state.newPassword]),
+      status: Formz.validate([password, state.oldPassword, state.confirmNew]),
     );
     emit(s);
   }
@@ -72,12 +107,107 @@ class ChangePasswordCubit extends Cubit<ChangePasswordState> {
     final confirmPassword =
         ConfirmPassword.dirty(password: state.newPassword.value, value: value);
     final s = state.copyWith(
-        confirmNew: confirmPassword.valid
-            ? confirmPassword
-            : ConfirmPassword.pure(
-                password: state.newPassword.value,
-                value: value,
-              ));
+      confirmNew: confirmPassword.valid
+          ? confirmPassword
+          : ConfirmPassword.pure(
+              password: state.newPassword.value,
+              value: value,
+            ),
+      status: Formz.validate([
+        state.oldPassword,
+        state.newPassword,
+        confirmPassword,
+      ]),
+    );
     emit(s);
+  }
+
+  void onOldPasswordUnfocused() {
+    final password = Password.dirty(state.oldPassword.value);
+    final s = state.copyWith(
+      oldPassword: password,
+      status: Formz.validate([
+        password,
+        state.newPassword,
+        state.confirmNew,
+      ]),
+    );
+    emit(s);
+  }
+
+  void onNewPasswordUnfocused() {
+    final password = Password.dirty(state.newPassword.value);
+    final s = state.copyWith(
+      newPassword: password,
+      status: Formz.validate([
+        state.oldPassword,
+        password,
+        state.confirmNew,
+      ]),
+    );
+    emit(s);
+  }
+
+  void onConfirmPasswordUnfocused() {
+    final confirmPassword = ConfirmPassword.dirty(
+      password: state.newPassword.value,
+      value: state.confirmNew.value,
+    );
+    final s = state.copyWith(
+      confirmNew: confirmPassword,
+      status: Formz.validate([
+        state.oldPassword,
+        state.newPassword,
+        confirmPassword,
+      ]),
+    );
+    emit(s);
+  }
+
+  void toggleOldPasswordObscure() {
+    emit(state.copyWith(oldPasswordObscured: !state.oldPasswordObscured));
+  }
+
+  void toggleNewPasswordObscure() {
+    emit(state.copyWith(newPasswordObscured: !state.newPasswordObscured));
+  }
+
+  void toggleConfirmPasswordObscure() {
+    emit(state.copyWith(
+        confirmPasswordObscured: !state.confirmPasswordObscured));
+  }
+
+  void onChangePassword() async {
+    final oldPassword = Password.dirty(state.oldPassword.value);
+    final newPassword = Password.dirty(state.newPassword.value);
+    final confirmPassword = ConfirmPassword.dirty(
+      password: newPassword.value,
+      value: state.confirmNew.value,
+    );
+    emit(state.copyWith(
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+      confirmNew: confirmPassword,
+      status: Formz.validate([
+        oldPassword,
+        newPassword,
+        confirmPassword,
+      ]),
+    ));
+
+    if (state.status.isValidated) {
+      emit(state.copyWith(loading: true, status: FormzStatus.submissionInProgress));
+      try {
+        await _repo.updateUserPassword(
+          oldPassword.value,
+          newPassword.value,
+          confirmPassword.value,
+        );
+        emit(state.copyWith(loading: false, status: FormzStatus.submissionSuccess));
+      } on Exception catch (e) {
+        _handler.handleExceptions(e);
+        emit(state.copyWith(loading: false, status: FormzStatus.submissionFailure));
+      }
+    }
   }
 }
--- lib/src/pages/password/views/change_password.dart
+++ lib/src/pages/password/views/change_password.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 import 'package:magic_express_delivery/src/app/app.dart';
 import 'package:magic_express_delivery/src/pages/pages.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class ChangePasswordPage extends StatelessWidget {
-
   static Route route() => AppRoutes.generateRoute(
-    child: ChangePasswordPage(),
-    fullScreenDialog: true,
-  );
+        child: ChangePasswordPage(),
+        fullScreenDialog: true,
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +22,7 @@ class ChangePasswordPage extends StatelessWidget {
       body: BlocProvider(
         create: (_) => ChangePasswordCubit(
           errorHandler: RepositoryProvider.of(context),
-          miscRepo: RepositoryProvider.of(context)
+          miscRepo: RepositoryProvider.of(context),
         ),
         child: _Form(),
       ),
@@ -35,16 +36,59 @@ class _Form extends StatefulWidget {
 }
 
 class _FormState extends State<_Form> {
+  final _oldPasswordNode = FocusNode();
+  final _newPasswordNode = FocusNode();
+  final _confirmPasswordNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _oldPasswordNode.addListener(() {
+      context.read<ChangePasswordCubit>().onOldPasswordUnfocused();
+    });
+
+    _newPasswordNode.addListener(() {
+      context.read<ChangePasswordCubit>().onNewPasswordUnfocused();
+    });
+
+    _confirmPasswordNode.addListener(() {
+      context.read<ChangePasswordCubit>().onConfirmPasswordUnfocused();
+    });
+  }
+
+  @override
+  void dispose() {
+    _oldPasswordNode.dispose();
+    _newPasswordNode.dispose();
+    _confirmPasswordNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        headerWidget(),
-        const SizedBox(height: 8.0),
-      ],
+    return BlocListener<ChangePasswordCubit, ChangePasswordState>(
+      listener: (context, state) {
+        if (state.status.isSubmissionSuccess) {
+          _SuccessDialogBuilder(
+            message: "Your password has been successfully changed!",
+          )..show(context);
+        }
+      },
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        children: [
+          const SizedBox(height: 16),
+          headerWidget(),
+          const SizedBox(height: 32),
+          _OldPasswordInput(_oldPasswordNode, _newPasswordNode),
+          const SizedBox(height: 16),
+          _NewPasswordInput(_newPasswordNode, _confirmPasswordNode),
+          const SizedBox(height: 16),
+          _ConfirmPasswordInput(_confirmPasswordNode),
+          const SizedBox(height: 48),
+          _SubmitButton(),
+        ],
+      ),
     );
   }
 
@@ -57,6 +101,367 @@ class _FormState extends State<_Form> {
             .textTheme
             .headline6
             ?.copyWith(fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+}
+
+class _OldPasswordInput extends StatelessWidget {
+  const _OldPasswordInput(this.node, [this.next]);
+
+  final FocusNode node;
+  final FocusNode? next;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChangePasswordCubit, ChangePasswordState>(
+      builder: (context, state) => TextFormField(
+        initialValue: state.oldPassword.value,
+        focusNode: node,
+        textInputAction: TextInputAction.next,
+        obscureText: state.oldPasswordObscured,
+        obscuringCharacter: '*',
+        onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(next),
+        onChanged: (password) {
+          context.read<ChangePasswordCubit>().onOldPasswordChanged(password);
+        },
+        cursorColor: Theme.of(context).primaryColorDark,
+        decoration: setInputDecoration(context, state.oldPasswordObscured),
+      ),
+    );
+  }
+
+  InputDecoration setInputDecoration(
+      BuildContext context, bool passwordObscured) {
+    final primaryColor = Theme.of(context).primaryColorDark;
+    final error =
+        context.select((ChangePasswordCubit b) => b.state.oldPassword).error;
+    return InputDecoration(
+      prefixIcon: Icon(Icons.password, color: primaryColor),
+      hintText: 'Current Password',
+      focusColor: primaryColor,
+      errorText: getError(error),
+      hintStyle: Theme.of(context)
+          .textTheme
+          .caption
+          ?.copyWith(fontWeight: FontWeight.w700),
+      suffixIcon: InkWell(
+        child: Icon(
+          passwordObscured ? MdiIcons.eye : MdiIcons.eyeOff,
+          color: primaryColor,
+        ),
+        onTap: () {
+          context.read<ChangePasswordCubit>().toggleOldPasswordObscure();
+        },
+      ),
+      focusedBorder: AppTheme.textOutlineFocusedBorder(context),
+      enabledBorder: AppTheme.textOutlineEnabledBorder(context),
+      errorBorder: AppTheme.textOutlineErrorBorder(context),
+      focusedErrorBorder: AppTheme.textOutlineFocusedBorder(context),
+    );
+  }
+
+  String? getError(PasswordValidationError? error) {
+    switch (error) {
+      case PasswordValidationError.invalid:
+        return 'Invalid password';
+      case PasswordValidationError.empty:
+        if (node.hasFocus)
+          return 'Enter a password';
+        else
+          return null;
+      default:
+        return null;
+    }
+  }
+}
+
+class _NewPasswordInput extends StatelessWidget {
+  const _NewPasswordInput(this.node, [this.next]);
+
+  final FocusNode node;
+  final FocusNode? next;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChangePasswordCubit, ChangePasswordState>(
+      builder: (context, state) => TextFormField(
+        initialValue: state.newPassword.value,
+        focusNode: node,
+        textInputAction: TextInputAction.next,
+        obscureText: state.newPasswordObscured,
+        obscuringCharacter: '*',
+        onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(next),
+        onChanged: (password) {
+          context.read<ChangePasswordCubit>().onNewPasswordChanged(password);
+        },
+        cursorColor: Theme.of(context).primaryColorDark,
+        decoration: setInputDecoration(context, state.newPasswordObscured),
+      ),
+    );
+  }
+
+  InputDecoration setInputDecoration(
+      BuildContext context, bool passwordObscured) {
+    final primaryColor = Theme.of(context).primaryColorDark;
+    final error =
+        context.select((ChangePasswordCubit b) => b.state.newPassword).error;
+    return InputDecoration(
+      prefixIcon: Icon(Icons.password, color: primaryColor),
+      hintText: 'New Password',
+      focusColor: primaryColor,
+      errorText: getError(error),
+      hintStyle: Theme.of(context)
+          .textTheme
+          .caption
+          ?.copyWith(fontWeight: FontWeight.w700),
+      suffixIcon: InkWell(
+        child: Icon(
+          passwordObscured ? MdiIcons.eye : MdiIcons.eyeOff,
+          color: primaryColor,
+        ),
+        onTap: () {
+          context.read<ChangePasswordCubit>().toggleNewPasswordObscure();
+        },
+      ),
+      focusedBorder: AppTheme.textOutlineFocusedBorder(context),
+      enabledBorder: AppTheme.textOutlineEnabledBorder(context),
+      errorBorder: AppTheme.textOutlineErrorBorder(context),
+      focusedErrorBorder: AppTheme.textOutlineFocusedBorder(context),
+    );
+  }
+
+  String? getError(PasswordValidationError? error) {
+    switch (error) {
+      case PasswordValidationError.invalid:
+        return 'Invalid password';
+      case PasswordValidationError.empty:
+        if (node.hasFocus)
+          return 'Enter a password';
+        else
+          return null;
+      default:
+        return null;
+    }
+  }
+}
+
+class _ConfirmPasswordInput extends StatelessWidget {
+  const _ConfirmPasswordInput(this.node);
+
+  final FocusNode node;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChangePasswordCubit, ChangePasswordState>(
+      builder: (context, state) => TextFormField(
+        initialValue: state.confirmNew.value,
+        focusNode: node,
+        textInputAction: TextInputAction.done,
+        obscureText: state.confirmPasswordObscured,
+        obscuringCharacter: '*',
+        onChanged: (password) {
+          context
+              .read<ChangePasswordCubit>()
+              .onConfirmPasswordChanged(password);
+        },
+        cursorColor: Theme.of(context).primaryColorDark,
+        decoration: setInputDecoration(context, state.confirmPasswordObscured),
+      ),
+    );
+  }
+
+  InputDecoration setInputDecoration(
+      BuildContext context, bool passwordObscured) {
+    final primaryColor = Theme.of(context).primaryColorDark;
+    final error =
+        context.select((ChangePasswordCubit b) => b.state.confirmNew).error;
+    return InputDecoration(
+      prefixIcon: Icon(Icons.password, color: primaryColor),
+      hintText: 'Confirm New Password',
+      focusColor: primaryColor,
+      errorText: getError(error),
+      hintStyle: Theme.of(context)
+          .textTheme
+          .caption
+          ?.copyWith(fontWeight: FontWeight.w700),
+      suffixIcon: InkWell(
+        child: Icon(
+          passwordObscured ? MdiIcons.eye : MdiIcons.eyeOff,
+          color: primaryColor,
+        ),
+        onTap: () {
+          context.read<ChangePasswordCubit>().toggleConfirmPasswordObscure();
+        },
+      ),
+      focusedBorder: AppTheme.textOutlineFocusedBorder(context),
+      enabledBorder: AppTheme.textOutlineEnabledBorder(context),
+      errorBorder: AppTheme.textOutlineErrorBorder(context),
+      focusedErrorBorder: AppTheme.textOutlineFocusedBorder(context),
+    );
+  }
+
+  String? getError(ConfirmPasswordValidator? error) {
+    switch (error) {
+      case ConfirmPasswordValidator.no_match:
+        return 'Password does not match';
+      case ConfirmPasswordValidator.empty:
+        if (node.hasFocus)
+          return 'Enter a password';
+        else
+          return null;
+      default:
+        return null;
+    }
+  }
+}
+
+class _SubmitButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+    return BlocBuilder<ChangePasswordCubit, ChangePasswordState>(
+      buildWhen: (p, c) => p.status != c.status,
+      builder: (context, state) {
+        return ElevatedButton(
+          child: Builder(
+            builder: (_) {
+              if (state.loading) {
+                return SizedBox.fromSize(
+                  child: CircularProgressIndicator.adaptive(
+                    valueColor: AlwaysStoppedAnimation<Color>(primaryColor),
+                  ),
+                  size: Size(22.0, 22.0),
+                );
+              }
+              return Text('Change Password');
+            },
+          ),
+          style: ElevatedButton.styleFrom(
+            textStyle: Theme.of(context)
+                .textTheme
+                .button
+                ?.copyWith(fontWeight: FontWeight.w700),
+            padding: EdgeInsets.all(16.0),
+            primary: primaryColorSelect(context),
+          ),
+          onPressed: state.status.isValidated
+              ? () {
+                  FocusScope.of(context).unfocus();
+                  context.read<ChangePasswordCubit>().onChangePassword();
+                }
+              : null,
+        );
+      },
+    );
+  }
+
+  Color primaryColorSelect(BuildContext context) {
+    final primaryColorDark = Theme.of(context).primaryColorDark;
+    final isLoading =
+        context.select((ChangePasswordCubit b) => b.state.loading);
+    if (isLoading) return Colors.transparent;
+    return primaryColorDark;
+  }
+}
+
+class _SuccessDialogBuilder extends StatelessWidget {
+  _SuccessDialogBuilder({required this.message, this.onRetry});
+
+  final String message;
+  final VoidCallback? onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(),
+        borderRadius: BorderRadius.all(
+          Radius.circular(8.0),
+        ),
+      ),
+      contentPadding: EdgeInsets.zero,
+      content: ConstrainedBox(
+        constraints:
+        BoxConstraints(minWidth: MediaQuery.of(context).size.width * 0.3),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 24.0),
+            Icon(Icons.check, size: 48),
+            const SizedBox(height: 24.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    message,
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1
+                        ?.copyWith(fontWeight: FontWeight.w700),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                    },
+                    child: Text('DISMISS'),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .button
+                          ?.copyWith(fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: onRetry != null,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            if (onRetry != null) {
+                              onRetry!();
+                            }
+                          },
+                          child: Text('RETRY'),
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .button
+                                ?.copyWith(fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void show(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => _SuccessDialogBuilder(
+        message: message,
+        onRetry: onRetry,
       ),
     );
   }
